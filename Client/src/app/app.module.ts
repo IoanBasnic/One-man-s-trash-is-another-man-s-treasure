c539eff6dbfc928bd14bf0dc79abe3fe249bea64
IoanBasnic <57294224+IoanBasnic@users.noreply.github.com>
2021-03-17 22:55:48 +0200

    Search box (#2)
    * Added client files
    
    * Added navigation bar for index.html
    
    * Added footer
    
    * Added some testing for body content
    
    * Added search function

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,6 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule} from '@angular/forms';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    Ng2SearchPipeModule
+=======
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -9,8 +32,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
