31fcc5940a4e11c41e0a197d5c3b8a09f63a7e6c
IoanBasnic <57294224+IoanBasnic@users.noreply.github.com>
2021-03-27 12:38:50 +0200

    init (#4)

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { HostListener } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -9,33 +9,45 @@ import { RegisterComponent } from './register/register.component';
 import {FormsModule} from '@angular/forms';
 
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    Ng2SearchPipeModule
-=======
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent },
+]; // sets up routes constant where you define your routes
 
-import { AppComponent } from './app.component';
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HomeComponent,
+//     AboutComponent,
+//     LoginComponent,
+//     RegisterComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//     Ng2SearchPipeModule,
+//     //RouterModule.forRoot([{path : 'register', component: RegisterComponent}], {enableTracing: true})
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
 
--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -10,6 +10,13 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const header = document.querySelector('nav');
+    const sectionOne = document.querySelector('.wrapper');
+
+    header.classList.add('nav-noscroll');
+    header.classList.remove('.navigation');
+
+
   }
 
 }
