9446c9eda3960bdbbd57e8db9b6a344f1f92ac45
IoanBasnic <57294224+IoanBasnic@users.noreply.github.com>
2021-04-28 21:32:42 +0300

    Server (#11)
    * Login/register api frontend<->backend
    
    * Added profile page
    
    * Added service server and added connection between the servers

---
+++ ServiceServer/src/main/java/OMTIAMT/serviceServer/Server/ServiceServerApplication.java
@@ -0,0 +1,41 @@
+package OMTIAMT.serviceServer.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class ServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceServerApplication.class, args);
+
+		int port = 6868;
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+			System.out.println("This server is listening on port " + port);
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Middleware server connected!");
+				OutputStream output = socket.getOutputStream();
+				PrintWriter writer = new PrintWriter(output, true);
+				writer.println(new Date().toString());
+			}
+
+		} catch (IOException ex) {
+			System.out.println("Server exception: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+}
