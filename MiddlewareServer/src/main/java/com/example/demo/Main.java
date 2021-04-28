aebc20e50d27a1ee092475d25b7f0ac480fb7eab
Basnic Ioan <basnic.ioan@gmail.com>
2021-04-28 21:24:18 +0300

    Added service server and added connection between the servers

--- MiddlewareServer/src/main/java/com/example/demo/Main.java
+++ MiddlewareServer/src/main/java/com/example/demo/Main.java
@@ -8,12 +8,42 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 @CrossOrigin(origins = "localhost:8080")
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+
+		String hostname = "localhost";
+		int port = 6868;
+
+
+		try (Socket socket = new Socket(hostname, port)) {
+
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			String time = reader.readLine();
+
+			System.out.println(time);
+
+
+		} catch (UnknownHostException ex) {
+
+			System.out.println("Server not found: " + ex.getMessage());
+
+		} catch (IOException ex) {
+
+			System.out.println("I/O error: " + ex.getMessage());
+		}
 	}
 
 }
