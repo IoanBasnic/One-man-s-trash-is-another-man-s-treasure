9d553c6b01b02f55bfa22107f07cdc4ded05f75f
Petrified0110 <[petra.horvath99@gmail.com]>
2021-05-03 22:51:31 +0300

    token template setup

--- MiddlewareServer/src/main/java/com/example/demo/ServiceControllers/ClientController.java
+++ MiddlewareServer/src/main/java/com/example/demo/ServiceControllers/ClientController.java
@@ -14,13 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 
 
