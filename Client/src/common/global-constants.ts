3fc42da7075ad4f05ac134fa25f785e0065bfd7b
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-06 21:38:26 +0300

    Changed URL

--- Client/src/common/global-constants.ts
+++ Client/src/common/global-constants.ts
@@ -4,10 +4,10 @@ import {Injectable} from '@angular/core';
   providedIn: 'root',
 })
 export class GlobalConstants {
-  public static apiURL = 'http://localhost:8080/';
+  public static apiURL = 'http://3.66.189.188:8080/';
   public productID = '';
   // tslint:disable-next-line:typedef
-  getPublicID() {return this.productID;}
+  getPublicID() {return this.productID; }
   // tslint:disable-next-line:typedef
   setPublicID(id: string) {this.productID = id; }
 }
