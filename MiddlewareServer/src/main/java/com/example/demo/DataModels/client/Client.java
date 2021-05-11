da55af58c7211570dc14020727e00979e968dc66
Petrified0110 <57863972+Petrified0110@users.noreply.github.com>
2021-05-11 22:38:34 +0300

    Implement client additional information endpoint (#24)
    * Implement client additional information endpoint
    
    * delete client
    
    Co-authored-by: Petrified0110 <[petra.horvath99@gmail.com]>

--- MiddlewareServer/src/main/java/com/example/demo/DataModels/client/Client.java
+++ MiddlewareServer/src/main/java/com/example/demo/DataModels/client/Client.java
@@ -1,27 +1,29 @@
 package com.example.demo.DataModels.client;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
 
 
 @Document(collection = "client")
-public class Client { //password encrypted
+public class Client {
     @Id
-    String id;
-
-    @Indexed(unique = true)
     String authId;
 
     String email;
 
-    public String getId() {
-        return id;
-    }
+    String givenName;
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    String familyName;
+
+    String nickname;
+
+    String phoneNumber;
+
+    String Address;
+
+    PaymentMethod paymentMethod;
 
     public String getAuthId() {
         return authId;
@@ -38,5 +40,94 @@ public class Client { //password encrypted
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return authId.equals(client.authId) &&
+                email.equals(client.email) &&
+                Objects.equals(givenName, client.givenName) &&
+                Objects.equals(familyName, client.familyName) &&
+                Objects.equals(nickname, client.nickname) &&
+                Objects.equals(phoneNumber, client.phoneNumber) &&
+                Objects.equals(Address, client.Address) &&
+                paymentMethod == client.paymentMethod;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authId, email, givenName, familyName, nickname, phoneNumber, Address, paymentMethod);
+    }
+
+    enum PaymentMethod{
+        CASH,
+        CARD,
+        PAYPAL,
+        OTHER
+    }
+
+    public void mapToPaymentMethod(String paymentMethod){
+        if(paymentMethod.equals("cash") || paymentMethod.equals("CASH") || paymentMethod.equals("Cash"))
+            this.paymentMethod = PaymentMethod.CASH;
+
+        if(paymentMethod.equals("card") || paymentMethod.equals("CARD") || paymentMethod.equals("Card"))
+            this.paymentMethod = PaymentMethod.CARD;
+
+        if(paymentMethod.equals("paypal") || paymentMethod.equals("PAYPAL") || paymentMethod.equals("Paypal"))
+            this.paymentMethod = PaymentMethod.PAYPAL;
+
+        this.paymentMethod = PaymentMethod.OTHER;
+    }
+
 }
 
--- MiddlewareServer/src/main/java/com/example/demo/ServiceControllers/ClientController.java
+++ MiddlewareServer/src/main/java/com/example/demo/ServiceControllers/ClientController.java
@@ -5,53 +5,75 @@ import com.example.demo.Repositories.ClientRepository;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Arrays;
 import java.util.Optional;
+
+import com.example.demo.HttpUtils.Auth0Utils;
 
 
 @CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+    private Auth0Utils auth0Utils = new Auth0Utils();
 
     @Autowired
     private ClientRepository clientRepository;
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public ResponseEntity add(@RequestBody Client client) throws IOException, InterruptedException {
+    public ResponseEntity add(@RequestBody String clientToken) throws IOException, InterruptedException {
+        JsonObject userInfo = auth0Utils.getUserInfo(clientToken);
 
-        Client savedClient;
+        if(userInfo.has("error"))
+            return ResponseEntity.status(500).body(userInfo.get("error"));
+
+        Optional<Client> existingClient = clientRepository.findById(userInfo.get("sub").toString());
+
+        if(existingClient.isEmpty()) {
+            Client savedClient = new Client();
+
+            try {
+                createClient(savedClient, userInfo);
+                clientRepository.save(savedClient);
+            } catch (DuplicateKeyException e) {
+                return ResponseEntity.status(400).body("{ \"message\": \"duplicate value\"}");
+            } catch (Exception e) {
+                return ResponseEntity.status(500).body(e);
+            }
+        }
+        else{
+            Client existingClientGet = existingClient.get();
+            updateClient(existingClientGet, userInfo);
+            clientRepository.save(existingClientGet);
+        }
+
+        return ResponseEntity.status(200).body("valid token acquired");
+    }
+
+    @PutMapping
+    public ResponseEntity addAdditionalValues(@RequestBody String clientToken, String additionalInfo) throws IOException, InterruptedException {
+        JsonObject userInfo = auth0Utils.getUserInfo(clientToken);
+        Optional<Client> existingClient = clientRepository.findById(userInfo.get("sub").toString());
+
+        if(existingClient.isEmpty())
+            return ResponseEntity.status(404).body("{ \"message\": \"client not found\"}");
+
+        JsonObject json = (JsonObject) JsonParser.parseString(additionalInfo);
+        Client client = existingClient.get();
+        updateClientAdditionalValues(client, json);
+
         try {
-            savedClient = clientRepository.save(client);
-
-            URL url = new URL("https://localhost:8080/register/sendmail?id=" + savedClient.getId());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            InputStream responseStream = connection.getInputStream();
-            System.out.println(Arrays.toString(responseStream.readAllBytes()));
-
-//            URL url = new URL("https://localhost:8080/register/sendmail?id=" + savedClient.getId());
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            InputStream responseStream = connection.getInputStream();
-//            System.out.println(Arrays.toString(responseStream.readAllBytes()));
-        } catch (DuplicateKeyException e){
-            return ResponseEntity.status(400).body("{ \"message\": \"duplicate value\"}");
-        } catch (Exception e){
+            clientRepository.save(client);
+        } catch (Exception e) {
             return ResponseEntity.status(500).body(e);
         }
 
-
-        return ResponseEntity.status(200).body(savedClient);
+        return ResponseEntity.status(200).body("valid token acquired");
     }
 
 
@@ -61,7 +83,15 @@ public class ClientController {
     }
 
 
-    private Boolean checkEmailValidity(String email) throws IOException, InterruptedException {
+    private Boolean checkEmailValidity(String email, Boolean emailVerified) throws IOException, InterruptedException {
+        if(emailVerified)
+            return true;
+
+//            URL url = new URL("https://localhost:8080/register/sendmail?id=" + savedClient.getId());
+//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//            InputStream responseStream = connection.getInputStream();
+//            System.out.println(Arrays.toString(responseStream.readAllBytes()));
+
         /* This is the email validator. The reason it is commented out is because I have subscribed to
          * an API that only lets us use the endpoint 1000 times a month before paying.
          * PLEASE DO NOT comment it out unless you want to demo or test it specifically.
@@ -81,6 +111,44 @@ public class ClientController {
 //        return jsonMap.get("valid").toString().equals("true");
 
         return true;
+    }
+
+    private void createClient(Client savedClient, JsonObject userInfo){
+        savedClient.setEmail(userInfo.get("email").toString());
+        savedClient.setAuthId(userInfo.get("sub").toString());
+
+        if(userInfo.has("given_name"))
+            savedClient.setGivenName(userInfo.get("given_name").toString());
+
+        if(userInfo.has("family_name"))
+            savedClient.setFamilyName(userInfo.get("family_name").toString());
+
+        if(userInfo.has("nickname"))
+            savedClient.setNickname(userInfo.get("nickname").toString());
+    }
+
+    private void updateClient(Client existingClient, JsonObject userInfo){
+        if(userInfo.has("given_name")){
+            existingClient.setGivenName(userInfo.get("given_name").toString());
+        }
+        if(userInfo.has("family_name")){
+            existingClient.setFamilyName(userInfo.get("family_name").toString());
+        }
+        if(userInfo.has("nickname")){
+            existingClient.setNickname(userInfo.get("nickname").toString());
+        }
+    }
+
+    private void updateClientAdditionalValues(Client client, JsonObject additionalInfo){
+        if(additionalInfo.has("phone_number")){
+            client.setPhoneNumber(additionalInfo.get("phone_number").toString());
+        }
+        if(additionalInfo.has("address")){
+            client.setAddress(additionalInfo.get("address").toString());
+        }
+        if(additionalInfo.has("payment_method")){
+            client.mapToPaymentMethod(additionalInfo.get("payment_method").toString());
+        }
     }
 }
 
