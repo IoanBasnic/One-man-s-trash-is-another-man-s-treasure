1544afb387426af2f7d6d34a9611afc81cce36dc
Petrified0110 <57863972+Petrified0110@users.noreply.github.com>
2021-05-23 21:50:27 +0300

    Resolve conflicts (#31)
    * Sending notification mail
    
    * Image upload
    
    Co-authored-by: Sebastian <sebi.atletico99@yahoo.com>
    Co-authored-by: Petrified0110 <[petra.horvath99@gmail.com]>

--- ServiceServer/src/main/java/OMTIAMT/serviceServer/Server/Controller/ImageController.java
+++ ServiceServer/src/main/java/OMTIAMT/serviceServer/Server/Controller/ImageController.java
@@ -1,9 +1,9 @@
 package OMTIAMT.serviceServer.Server.Controller;
 
-
-
 import OMTIAMT.serviceServer.Server.Model.ImgurRes;
 import OMTIAMT.serviceServer.Server.Service.ImageService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.lang.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,11 +11,13 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.SocketUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.websocket.server.PathParam;
 import java.io.IOException;
 import java.nio.file.Files;
 
@@ -26,60 +28,65 @@ public class ImageController {
     @Autowired
     public ImageService imageService;
 
+    private String productId;
+
+
     @PostMapping("/uploadFile")
-    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile[] file) throws Exception {
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
         try {
-            for(MultipartFile f : file){
-                String fileName = StringUtils.cleanPath(f.getOriginalFilename());
-                System.out.println(fileName);
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            System.out.println(fileName);
 
-                byte[] fileContent = f.getBytes();
+            byte[] fileContent = file.getBytes();
 
-                if(fileContent != null){
-                    System.out.println("File is not empty");
-                }
-
-                System.out.println(writeToStore(fileContent));
+            if (fileContent == null) {
+                return new ResponseEntity<>("File empty", HttpStatus.NOT_ACCEPTABLE);
             }
 
+            ResponseEntity<ImgurRes> response = imageService.writeToStore(fileContent);
+
+            if (!(response.getStatusCodeValue() == 200)) {
+                return new ResponseEntity<>("Something happened while uploading image", HttpStatus.BAD_REQUEST);
+            }
+
+            String image = response.getBody().getData().getLink();
+
+            ResponseEntity responseEntity = imageService.sendImage(this.productId,image);
+
             return new ResponseEntity<>("OK", HttpStatus.OK);
-        }catch (Exception e){
-            return new ResponseEntity<>("File type not accepted",HttpStatus.BAD_REQUEST);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>("File type not accepted", HttpStatus.BAD_REQUEST);
         }
-//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//        System.out.println(fileName);
-//
-//        byte[] fileContent = file.getBytes();
-//
-//        if(fileContent != null){
-//            System.out.println("File is not empty");
-//        }
-//
-//        System.out.println(writeToStore(fileContent));
-
-//        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
-
-    public String writeToStore(byte[] fileBytes) throws Exception {
-        String clientId="69590164bdb6010";
-        final String uri = "https://api.imgur.com/3/image";
-
-        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-        bodyMap.add("image", getUserFileResource(fileBytes));
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.add("Authorization", "Client-ID "+clientId);
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<ImgurRes> response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, ImgurRes.class);
-        System.out.println("response status: " + response.getStatusCode()); // it should return 200
-        System.out.println("response body: " + response.getBody().getData().getLink()); // it should return link of your uploaded image
-
-        return response.getBody().getData().getLink();
-    }
-
     public static ByteArrayResource getUserFileResource(byte[] bytes) throws IOException {
         return new ByteArrayResource(bytes);
+    }
+
+    @GetMapping(value="/checkProduct")
+    public ResponseEntity<Object> productExistById(@RequestBody String data){
+
+        JsonObject dataJson = (JsonObject) JsonParser.parseString(data);
+        String clientToken = dataJson.get("clientToken").getAsString();
+        this.productId = dataJson.get("productId").getAsString();
+
+        ResponseEntity responseEntity = imageService.checkProduct(productId,clientToken);
+
+        System.out.println(getProductId());
+
+        return responseEntity;
+    }
+
+    public String getProductId(){
+        return this.productId;
+    }
+
+    @GetMapping(params = {"productId", "clientToken"})
+    public ResponseEntity<Object> productExistByTokenAndId(@PathParam("productId") String productId, String clientToken){
+
+        System.out.println(productId);
+        System.out.println(clientToken);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- ServiceServer/src/main/java/OMTIAMT/serviceServer/Server/Service/ImageService.java
+++ ServiceServer/src/main/java/OMTIAMT/serviceServer/Server/Service/ImageService.java
@@ -1,54 +1,101 @@
 package OMTIAMT.serviceServer.Server.Service;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.http.ResponseEntity;
+import OMTIAMT.serviceServer.Server.Model.ImgurRes;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
 public class ImageService {
 
-   // public final String storageDirectoryPath = "C:\\Users\\Ahmed\\Desktop\\mediumImages";
+    public static ByteArrayResource getUserFileResource(byte[] bytes) throws IOException {
+        return new ByteArrayResource(bytes);
+    }
 
-    public ResponseEntity uploadToLocalFileSystem(MultipartFile file) {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    public  ResponseEntity<ImgurRes> writeToStore(byte[] fileBytes) throws Exception {
+        String clientId="69590164bdb6010";
+        final String uri = "https://api.imgur.com/3/image";
 
-//        Path storageDirectory = Paths.get(storageDirectoryPath);
-//
-//        if(!Files.exists(storageDirectory)){
-//            try {
-//                Files.createDirectories(storageDirectory);
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        Path destination = Paths.get(storageDirectory.toString() + "\\" + fileName);
-//
-//        try {
-//            Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("api/images/getImage/")
-//                .path(fileName)
-//                .toUriString();
-//
-        return ResponseEntity.ok("OK");
+        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("image", getUserFileResource(fileBytes));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.add("Authorization", "Client-ID "+clientId);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<ImgurRes> response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, ImgurRes.class);
+        System.out.println("response status: " + response.getStatusCode()); // it should return 200
+        System.out.println("response body: " + response.getBody().getData().getLink()); // it should return link of your uploaded image
+
+        return response;
+    }
+
+    public ResponseEntity<Object> sendImage(String productId, String image){
+
+        final String uri = "https://www.covidtector.tk:8080/product?productId={id}&image={image}";
+        Map<String, String> vars = new HashMap<>();
+        vars.put("id", productId);
+        vars.put("image", image);
+
+        System.out.println(productId);
+        System.out.println(image);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            ResponseEntity responseEntity = restTemplate.postForObject(uri, vars, ResponseEntity.class);
+
+        } catch (HttpClientErrorException | HttpServerErrorException httpClientOrServerExc) {
+
+            if (HttpStatus.NOT_FOUND.equals(httpClientOrServerExc.getStatusCode())) {
+
+                System.out.println("NOT FOUND");
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> checkProduct(String productId, String clientToken){
+
+        final String uri = "https://www.covidtector.tk:8080/product?productId={id}";
+        Map<String, String> vars = new HashMap<>();
+        vars.put("id", productId);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(clientToken);
+
+        HttpEntity entity = new HttpEntity(headers);
+        ResponseEntity responseEntity;
+
+        try {
+            responseEntity = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class, vars);
+        } catch (HttpClientErrorException | HttpServerErrorException httpClientOrServerExc) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if (HttpStatus.NOT_FOUND.equals(responseEntity.getStatusCode())) {
+
+            System.out.println("NOT FOUND");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
